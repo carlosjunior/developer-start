8b95656142be790dd91f4b98c03f221a42028d35
Carlos Souza <csouza@fluig.com>
2018-04-27 14:54:48 -0300

    commit initial

---
+++ developer-start.sh
@@ -0,0 +1,72 @@
+echo ==============================
+echo update ubuntu package
+echo ==============================
+sudo apt-get update && \
+
+echo ==============================
+echo install jdk
+echo ==============================
+sudo add-apt-repository ppa:webupd8team/java && \
+sudo apt-get install oracle-java8-installer && \
+sudo java -version && \
+
+echo ==============================
+echo install maven
+echo ==============================
+sudo apt-get install maven && \
+sudo mvn --version && \
+
+echo ==============================
+echo install git
+echo ==============================
+sudo apt-get install git && \
+git --version && \
+
+echo ==============================
+echo config git
+echo ==============================
+echo git name, example: Lord Voldemort
+read gitName
+git config --global user.name $gitName  && \
+echo git email, example: lord.voldemort@fluig.com
+read gitEmail
+git config --global user.email $gitEmail  && \
+git config --global core.precomposeunicode true  && \
+git config --list && \
+
+echo ==============================
+echo git ssh key
+echo ==============================
+ssh-keygen -t rsa -C $gitEmail && \
+
+echo ==============================
+echo install docker
+echo ==============================
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common && \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
+sudo apt-key fingerprint 0EBFCD88 && \
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu artful stable" && \
+sudo apt-get update && \
+sudo apt-get install docker-ce && \
+sudo docker --version && \
+
+echo ==============================
+echo install docker-compose
+echo ==============================
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose && \
+sudo chmod +x /usr/local/bin/docker-compose && \
+docker-compose --version && \
+
+echo ==============================
+echo install node
+echo ==============================
+sudo apt-get install nodejs && \
+
+echo ==============================
+echo install npm
+echo ==============================
+sudo apt-get install npm && \
+
+echo ==============================
+echo Done, restart your machine!
+echo ==============================

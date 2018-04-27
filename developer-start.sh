e89b28ff21f70aef450a580df710c4f426e0baa0
Lucas Vieira <lnvieira@users.noreply.github.com>
2018-04-27 15:56:00 -0300

    Update developer-start.sh

--- developer-start.sh
+++ developer-start.sh
@@ -58,6 +58,12 @@ apt-get update && \
 apt-get install -y docker-ce
 
 echo ==============================
+echo add docker root
+echo ==============================
+groupadd docker && \
+usermod -aG docker $USER
+
+echo ==============================
 echo install docker-compose
 echo ==============================
 curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose && \

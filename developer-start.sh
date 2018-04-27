99b9021f831b5a83dc9e705fe757e44fc3c0f595
Carlos <carlosjuniorbbs@gmail.com>
2018-04-27 15:26:02 -0300

    removido espaços

--- developer-start.sh
+++ developer-start.sh
@@ -1,37 +1,36 @@
 echo ==============================
 echo update ubuntu package 
 echo ============================== 
-apt-get update && \
+apt-get update
 
 echo ==============================
 echo install jdk
 echo ==============================
 add-apt-repository ppa:webupd8team/java && \
 apt-get install oracle-java8-installer && \
-java -version && \
+java -version
 
 echo ==============================
 echo install maven
 echo ==============================
 cd /opt/ wget http://www-eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz && \
 tar -xvzf apache-maven-3.3.9-bin.tar.gz && \
-mv apache-maven-3.3.9 maven && \
+mv apache-maven-3.3.9 maven
 
 echo ================================
 echo Adicionado variável de ambiente
 echo ================================
 echo "export M2_HOME=/opt/maven" > /etc/profile.d/mavenenv.sh && \
 echo "export PATH=${M2_HOME}/bin:${PATH}" >> /etc/profile.d/mavenenv.sh && \
-
 chmod +x /etc/profile.d/mavenenv.sh && \
 source /etc/profile.d/mavenenv.sh && \
-mvn --version && \
+mvn --version
 
 echo ==============================
 echo install git
 echo ==============================
 apt-get install git && \
-git --version && \
+git --version
 
 echo ==============================
 echo config git
@@ -43,12 +42,12 @@ echo git email, example: lord.voldemort@fluig.com
 read gitEmail
 git config --global user.email $gitEmail  && \
 git config --global core.precomposeunicode true  && \
-git config --list && \
+git config --list
 
 echo ==============================
 echo git ssh key
 echo ==============================
-ssh-keygen -t rsa -C $gitEmail && \
+ssh-keygen -t rsa -C $gitEmail
 
 echo ==============================
 echo install docker
@@ -59,24 +58,24 @@ apt-key fingerprint 0EBFCD88 && \
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu artful stable" && \
 apt-get update && \
 apt-get install docker-ce && \
-docker --version && \
+docker --version
 
 echo ==============================
 echo install docker-compose
 echo ==============================
 curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose && \
 chmod +x /usr/local/bin/docker-compose && \
-docker-compose --version && \
+docker-compose --version
 
 echo ==============================
 echo install node
 echo ==============================
-apt-get install nodejs && \
+apt-get install nodejs
 
 echo ==============================
 echo install npm
 echo ==============================
-apt-get install npm && \
+apt-get install npm
 
 echo ==============================
 echo Done, restart your machine!
